b6fc6ebae0a5129049d612ac573b2e09f8ffa821
themandalorian <dwarfoffroad@protonmail.com>
2025-05-07 12:14:56 -0300

    #c19 feat: aux page structure

--- src/pages/Page/Page.jsx
+++ src/pages/Page/Page.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { CiCircleInfo } from "react-icons/ci";
 
 function Page() {
-  return <h1>Page Page</h1>;
+  return (
+    <main className="page">
+      <section className="page__header">
+        <h1 className="page__title">Nossa história</h1>
+        <p className="page__subtitle">
+          <CiCircleInfo /> Se apaixone pela nossa história
+        </p>
+      </section>
+
+      <section className="page__content">
+        <p>
+          Esta página pode ser usada para mostrar mais detalhes sobre eventos,
+          campeonatos, treinamentos ou outras atividades do São Paulo Giants.
+        </p>
+        <p>
+          Sinta-se à vontade para modificar este conteúdo conforme necessário. Aqui é um
+          bom lugar para colocar notícias, regulamentos ou galerias de fotos.
+        </p>
+      </section>
+    </main>
+  );
 }
 
 export default Page;
