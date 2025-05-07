25816d0893e89e1e61f448815e6e38cd51425df6
themandalorian <dwarfoffroad@protonmail.com>
2025-05-07 12:33:40 -0300

    #c22 refactor: header code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,6 +7,18 @@ function Header() {
 
     const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
+    const pages = [
+        { href: "", icon: <BsFillHousesFill />, label: "Home" },
+        { href: "#", icon: <BsBookmarkHeart />, label: "Formulário" },
+        { href: "#about", icon: <BsFileText />, label: "Sobre" },
+        { href: "", icon: <BsPatchQuestion />, label: "Ajuda" },
+    ];
+
+    const socialLinks = [
+        { href: "#", icon: <FaInstagram /> },
+        { href: "#", icon: <RiFacebookCircleLine /> },
+        { href: "#", icon: <BsWhatsapp /> },
+    ];
 
     return (
 
@@ -17,32 +29,34 @@ function Header() {
                     <a href="">giants.com.br</a>
                 </div>
             </div>
-
             <div className="header__bottom">
                 <div className="header__logo">
                     <img src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=97,h=41,fit=crop,trim=0;0;13.738095238095237;0/YrDXLjN9Kgf9k0GL/logo-gts-AGBvrJ7MzkUq35e4.png" alt="" srcSet="" />
                 </div>
-
-                {/* Botão do menu mobile */}
                 <button
                     className="header__mobile-toggle"
                     onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
                 >
                     {isMobileMenuOpen ? <BsX /> : <BsList />}
                 </button>
-
-                {/* Menu principal */}
                 <nav className={`header__nav ${isMobileMenuOpen ? 'is-active' : ''}`}>
                     <ul className="header__list header__list--pages">
-                        <li className="header__item"><a href="" className="header__anchor"><BsFillHousesFill />Home</a></li>
-                        <li className="header__item"><a href="#" className="header__anchor"><BsBookmarkHeart />Formulário</a></li>
-                        <li className="header__item"><a href="#about" className="header__anchor"><BsFileText />Sobre</a></li>
-                        <li className="header__item"><a href="" className="header__anchor"><BsPatchQuestion />Ajuda</a></li>
+                        {pages.map((page, index) => (
+                            <li className="header__item" key={index}>
+                                <a href={page.href} className="header__anchor">
+                                    {page.icon}{page.label}
+                                </a>
+                            </li>
+                        ))}
                     </ul>
                     <ul className="header__list header__list--social">
-                        <li className="header__item"><a className="header__anchor" href="#"><FaInstagram /></a></li>
-                        <li className="header__item"><a className="header__anchor" href="#"><RiFacebookCircleLine /></a></li>
-                        <li className="header__item"><a className="header__anchor" href="#"><BsWhatsapp /></a></li>
+                        {socialLinks.map((social, index) => (
+                            <li className="header__item" key={index}>
+                                <a className="header__anchor" href={social.href}>
+                                    {social.icon}
+                                </a>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
             </div>
