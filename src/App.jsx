7646b512672359c43f553d1b76dc8cb2877f5bdc
themandalorian <dwarfoffroad@protonmail.com>
2025-05-07 12:25:22 -0300

    #c20 feat: mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -19,72 +19,50 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Page from "./pages/Page"
 import Home from "./pages/Home"
 
+import { BsList, BsX } from 'react-icons/bs'; // Ícones para abrir/fechar menu
+
 function App() {
 
-
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
     return (
         <>
 
             <header className="header">
                 <div className="header__top">
-                    <div className="header__name">
-                        São Paulo Giants Beiseball & Softball
-                    </div>
+                    <div className="header__name">São Paulo Giants Beiseball & Softball</div>
                     <div className="header__website">
-                        <a href="">
-                            giants.com.br
-                        </a>
+                        <a href="">giants.com.br</a>
                     </div>
                 </div>
+
                 <div className="header__bottom">
-                    <div className="header__logo">
+                <div className="header__logo">
                         <img src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=97,h=41,fit=crop,trim=0;0;13.738095238095237;0/YrDXLjN9Kgf9k0GL/logo-gts-AGBvrJ7MzkUq35e4.png" alt="" srcSet="" />
                     </div>
-                    <nav className="header__nav">
+
+                    {/* Botão do menu mobile */}
+                    <button
+                        className="header__mobile-toggle"
+                        onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+                    >
+                        {isMobileMenuOpen ? <BsX /> : <BsList />}
+                    </button>
+
+                    {/* Menu principal */}
+                    <nav className={`header__nav ${isMobileMenuOpen ? 'is-active' : ''}`}>
                         <ul className="header__list header__list--pages">
-                            <li className="header__item">
-                                <a href="" className="header__anchor">
-                                    <BsFillHousesFill />Home
-                                </a>
-                            </li>
-                            <li className="header__item">
-                                <a href="#" className="header__anchor">
-                                    <BsBookmarkHeart />Formulário
-                                </a>
-                            </li>
-                            <li className="header__item">
-                                <a href="#about" className="header__anchor">
-                                    <BsFileText />
-                                    Sobre
-                                </a>
-                            </li>
-                            <li className="header__item">
-                                <a href="" className="header__anchor">
-                                    <BsPatchQuestion />Ajuda
-                                </a>
-                            </li>
+                            <li className="header__item"><a href="" className="header__anchor"><BsFillHousesFill />Home</a></li>
+                            <li className="header__item"><a href="#" className="header__anchor"><BsBookmarkHeart />Formulário</a></li>
+                            <li className="header__item"><a href="#about" className="header__anchor"><BsFileText />Sobre</a></li>
+                            <li className="header__item"><a href="" className="header__anchor"><BsPatchQuestion />Ajuda</a></li>
                         </ul>
                         <ul className="header__list header__list--social">
-                            <li className="header__item">
-                                <a className="header__anchor" href="http://" target="_blank" rel="noopener noreferrer">
-                                    <FaInstagram />
-                                </a>
-                            </li>
-                            <li className="header__item">
-                                <a className="header__anchor" href="http://" target="_blank" rel="noopener noreferrer">
-                                    <RiFacebookCircleLine />
-                                </a>
-                            </li>
-                            <li className="header__item">
-                                <a className="header__anchor" href="http://" target="_blank" rel="noopener noreferrer">
-                                    <BsWhatsapp />
-                                </a>
-                            </li>
+                            <li className="header__item"><a className="header__anchor" href="#"><FaInstagram /></a></li>
+                            <li className="header__item"><a className="header__anchor" href="#"><RiFacebookCircleLine /></a></li>
+                            <li className="header__item"><a className="header__anchor" href="#"><BsWhatsapp /></a></li>
                         </ul>
                     </nav>
-                </div>
-                <div className="header__container">
                 </div>
             </header>
 
